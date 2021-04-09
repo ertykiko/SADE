80ab819b906008f7fbe66ebcfc81b676f5562cb4
Francisco Rodriguess <franciscorodrigues1999@gmail.com>
2021-04-09 10:10:51 +0100

    Tkinter implementation

--- sade.py
+++ sade.py
@@ -1,12 +1,23 @@
-# %%
+
 import pandas as pd
 import numpy as np
 import tkinter as tk
 
 root = tk.Tk()
 
+def click_storage():
+        label = tk.Label(root, text="Magic has been made")
+        label.pack()
+
+
 w = tk.Label(root, text="Hello Tkinter!")
-w.pack()
+k = tk.Label(root, text="My name is Rodas Rolamentos")
+but = tk.Button(root, text="Adicionar armazem", command=click_storage)
+
+k.grid(row=0, column=0)
+w.grid(row=1, column=0)
+but.grid(row=2, column=0)
+
 
 root.mainloop()
 
@@ -14,18 +25,11 @@ root.mainloop()
 
 
 
-# %%
+
 
 clients_df = pd.read_csv("clients.txt",sep=';',header=0)
 
 warehouses_df = pd.read_csv("warehouses.txt",sep=';',header=0)
-# %%
 clients_df
 
-
-# %%
-warehouses_df
-
-
-
-
+print(warehouses_df)
