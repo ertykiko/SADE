00ae5c6823b9619006e26b5c1398d08ed9798eba
Francisco Rodriguess <franciscorodrigues1999@gmail.com>
2021-04-28 14:05:11 +0100

    Work from Pratical class

--- sade.py
+++ sade.py
@@ -45,20 +45,29 @@ def get_cost(state):
     raise NotImplementedError
 
 
+def order_route(routes,cli_arr,war_arr):
+    ##get the smallest distance
+
+    for client in range(len(routes[warehouse])):
+        ##Calculate distance to warehouse form all clients
+        distance[client] = get_distance(cli_arr[route[warehouse][client]], war_arr[warehouse, ])
+        #order by increasing
+
+
 def get_neighbors(state):
     """Returns neighbors of the argument state for your solution."""
+    
+        
+
     raise NotImplementedError
 
 
 def get_total_cost(routes, cli_arr, war_arr):
-      start = timer()
+      
       cost=0
       for warehouse in range(16):
             for client in range(20):
-                if len(routes[warehouse]) == client  :
-                    
-               # if math.isnan(routes[warehouse][client]) == True :
-                    
+                if len(routes[warehouse]) == client  : 
                     # Finished the end of the route or no stores in route
                     if client == 0:
                         #route is empty --  no cost
@@ -81,8 +90,7 @@ def get_total_cost(routes, cli_arr, war_arr):
                     print("Client : " + str(client))
                     cost += get_distance(cli_arr[routes[warehouse][client], ], cli_arr[routes[warehouse][client-1], ])
                     #cost =+ calculate_cost_between_stores
-      end = timer()
-      print("Time elapsed: " + str(end - start))
+      
       return cost
     #raise NotImplementedError
 
@@ -97,27 +105,45 @@ def check_routes(routes):
     for warehouse in range(16):
         print(routes[warehouse])
 
-def initialize_routes(routes):
+def initialize_routes(routes,war_arr,cli_arr,cli_dem,war_cap):
     vector_stores = list(range(0, 86))
-    random.shuffle(vector_stores)
+    #random.shuffle(vector_stores)
     #print(vector_stores)
     i = 0
     j = 0
     while i<len(vector_stores):
         if j % 16 == 0 and i != 0:
             j = 0
-        print('Checking store ', i, 'with demand ',cli_dem[vector_stores[i]] ,'to append on warehouse ',j, 'with free capacity ', war_cap[j]-sum(cli_dem[routes[j]]))
+        print('Checking store ',vector_stores[i], 'with demand ',cli_dem[vector_stores[i]] ,'to append on warehouse ',j, 'with free capacity ', war_cap[j]-sum(cli_dem[routes[j]]))
         if war_cap[j]>=sum(cli_dem[routes[j]])+cli_dem[vector_stores[i]]:
             routes[j].append(vector_stores[i])
-            print('Store', i,'with index',vector_stores[i],'attributed to WH',j,'\n')
+            print('Store', vector_stores[i],'with index',vector_stores[i],'attributed to WH',j,'\n')
             i=i+1
         j=j+1 
-            
+    war_distance[16]={0}
+    for stores in vector_stores:
+        for warehouse in range(16):
+            if(warehouse == 0 ):
+                distance =  get_distance(war_arr[warehouse, ], cli_arr[stores,])
+                choice = warehouse
+            else :
+                new_distance =  get_distance(war_arr[warehouse, ], cli_arr[stores, ])
+                if(new_distance < distance):
+                    distance = new_distance
+                    choice = warehouse
+        if(distance < war_distance[choice] or war_distance[choice] == 0) :
+            #Do nothing    
+            war_distance[choice]=distance
+
+              
+
+
+
 #################
 
 
 ##----Import data from CSV----##
-
+start = timer()
 clients_df = pd.read_csv("clients.txt",sep=';',header=0)
 warehouses_df = pd.read_csv("warehouses.txt",sep=';',header=0)
 cli_arr = clients_df[['XX', 'YY']].to_numpy()
@@ -144,8 +170,8 @@ print(war_arr)
 ### Array
 ### 1st position is the store / warehouse
 ### 2nd position is the dimension ---- 0 - XX and 1 - YY
-print(war_arr[1, ])
-print(cli_arr[0,] )
+# print(war_arr[1, ])
+# print(cli_arr[0,] )
 #distance = np.linalg.norm(cli_arr[1,] - cli_arr[0,])
 #distance = np.linalg.norm(war_arr[1, ] - cli_arr[0, ])
 
@@ -154,20 +180,32 @@ print(cli_arr[0,] )
 ###################################################################################
 routes = [[] for i in range(0,16)] 
 
-initialize_routes(routes)
-check_routes(routes)
+
+war_distance = np.zeros(16,int)
+
+print(war_distance)
+war_distance[0]=9
+print(war_distance)
+print(war_distance[0])
+
+
+
+#initialize_routes(routes)
+#check_routes(routes)
 
 
 #######################################################################################################
 
-total_cost = 0
+#total_cost = 0
 
 #16 vetores de 20 posições
-total_cost = get_total_cost(routes,cli_arr,war_arr)
+#total_cost = get_total_cost(routes,cli_arr,war_arr)
 
 
-print("Total cost is " + str(total_cost) )
+#print("Total cost is " + str(total_cost) )
 
+#end = timer()
+#print("Time elapsed: " + str(end - start))
 ####To-do 
     # Manually add stores to routes vector and test get total cost function
 
